0128eec2c63cd806c4f3a5e06546d49d7460e621
danse <f.occhipinti@gmail.com>
2018-02-26 17:31:41 +0100

    add a bash script that translates all documents

---
+++ update-all.sh
@@ -0,0 +1,6 @@
+k=( 18app carta-docente )
+cd ~/docs2rst/
+filters=$(ls filters/ | while read f; do echo -n " --filter filters/${f}"; done)
+a="--wrap none ${filters}"
+d=input-docx; for s in ${k[@]}; do c=$d/$s/; ls $c | while read f; do ./fork $c/$f $a -o output-rst/$s/${f%.docx}.rst; done; done;
+
