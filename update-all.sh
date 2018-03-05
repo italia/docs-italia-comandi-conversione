2733593195559fc54e8c0bf86a95f5a24e0194ed
danse <f.occhipinti@gmail.com>
2018-03-05 17:51:53 +0100

    improve readability and robustness

--- update-all.sh
+++ update-all.sh
@@ -2,5 +2,13 @@ k=$( ls input-docx )
 cd ~/docs2rst/
 filters=$(ls filters/ | while read f; do echo -n " --filter filters/${f}"; done)
 a="--wrap none ${filters}"
-d=input-docx; for s in ${k[@]}; do c=$d/$s/; ls $c | while read f; do ./fork $c/$f $a -o output-rst/$s/${f%.docx}.rst; done; done;
+for s in ${k[@]}
+do
+    mkdir -p "output-rst/$s/"
+    c="input-docx/$s/"
+    ls "$c" | while read f
+    do
+        ./fork "$c/$f" $a -o "output-rst/$s/${f%.docx}.rst"
+    done
+done
 
