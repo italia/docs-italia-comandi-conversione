56eaca0aeb53e4bf6e883b758e3cd52c89217162
danse <f.occhipinti@gmail.com>
2018-03-06 12:02:08 +0100

    add simple logic for error aggregation

--- syntax-check.sh
+++ syntax-check.sh
@@ -1,1 +1,9 @@
-lt output-rst/*/* | while read f; do rst2html "$f" > /dev/null ; done
+e=/tmp/syntax-check-errors
+: > $e
+lt output-rst/*/* | while read f
+do
+    rst2html "$f" > /dev/null 2>> $e
+done
+cut -d':' -f1,3 < $e | sort | uniq -c
+echo "errors details in $e"
+
