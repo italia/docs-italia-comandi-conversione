0a05f7351451954e323cf8b8a035e5e541999b65
danse <f.occhipinti@gmail.com>
2018-03-07 12:03:29 +0100

    port part of the automation logic to haskell to better manage complexity

--- update-all.sh
+++ update-all.sh
@@ -1,14 +1,10 @@
 k=$( ls input-docx )
-cd ~/docs2rst/
-filters=$(ls filters/ | while read f; do echo -n " --filter filters/${f}"; done)
-a="--wrap none ${filters}"
 for s in ${k[@]}
 do
     mkdir -p "output-rst/$s/"
-    c="input-docx/$s/"
-    ls "$c" | while read f
+    ls input-docx/$s/* | while read f
     do
-        ./fork "$c/$f" $a -o "output-rst/$s/${f%.docx}.rst"
+        ./translate -d "$f" -t rst
     done
 done
 
