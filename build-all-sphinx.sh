856770b99da43c70267c257a0af52404037a7b94
danse <f.occhipinti@gmail.com>
2018-04-17 15:25:24 +0200

    extract the buildExistingSphinx function

--- build-all-sphinx.sh
+++ build-all-sphinx.sh
@@ -1,9 +1,2 @@
-buildExistingSphinx () {
-    mv index.rst /tmp/build-existing-sphinx.rst
-    sphinx-quickstart -p test -a test -v test -q
-    mv -f /tmp/build-existing-sphinx.rst index.rst
-    make html
-    echo "make html"
-    firefox _build/html/index.html &
-}
+source build-existing-sphinx.sh
 ls output/*/* -d | while read d; do cd "$d"; buildExistingSphinx ; cd -; done
---
+++ build-existing-sphinx.sh
@@ -0,0 +1,8 @@
+buildExistingSphinx () {
+    mv index.rst /tmp/build-existing-sphinx.rst
+    sphinx-quickstart -p test -a test -v test -q
+    mv -f /tmp/build-existing-sphinx.rst index.rst
+    make html
+    echo "make html"
+    firefox _build/html/index.html &
+}
