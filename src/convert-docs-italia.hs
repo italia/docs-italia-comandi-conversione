fcdc51a5da45bf5dfbbe7bfcf4cf17382ecdc26b
danse <f.occhipinti@gmail.com>
2018-05-21 21:21:49 +0200

    refactor the conversion code

--- src/convert-docs-italia.hs
+++ src/convert-docs-italia.hs
@@ -1,8 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Turtle hiding (splitDirectories, replaceExtensions, stdout, stderr)
-import Data.List (intersperse)
-import Data.Text (pack, unpack)
+import Turtle hiding (splitDirectories,
+                      replaceExtensions,
+                      stdout,
+                      stderr,
+                      FilePath(..)
+                     )
+import Data.Text (pack, unpack, intercalate)
 import System.FilePath.Posix
 import System.Directory
 
@@ -11,22 +15,24 @@ parser = argText "doc" "document to convert"
         --   <*> optText "to" 't' "destination format"
 
 
-offset = "../../../" :: String
+offset = "../../../"
 
-filters = concat $ intersperse " " $ map addFilter files
+filters = intercalate " " $ map addFilter files
   where files = [ "add-headers.hs"
                 , "figure.hs" -- before divs are removed
                 , "remove-divs.hs"
                 , "remove-quotes.hs"
-                ]
+                ] :: [Text]
 
+addFilter :: Text -> Text
 addFilter f = "--filter " <> offset <> "pandoc-filters/filters/" <> f
 
 -- options to use every time we write an RST
+writeOpts :: Text
 writeOpts = "--wrap none " <> addFilter "loosen-lists.hs" <> " --standalone"
 
+opts :: Text
 opts = writeOpts <> " --extract-media . " <> filters <> "  -f docx+styles"
-
 
 -- | convert the input file to the output folder
 --
@@ -34,67 +40,68 @@ opts = writeOpts <> " --extract-media . " <> filters <> "  -f docx+styles"
 -- "output/otherdir/file"
 fileToFolder i = joinPath ["output", firstParent i, takeBaseName i]
   where firstParent = head . drop 1 . reverse . splitDirectories
-
 -- | move the input file to the destination folder
 --
 -- >>> inputName "somedir/otherdir/file.ext"
 -- "input.ext"
+inputName :: FilePath -> FilePath
 inputName i = addExtension "input" (takeExtension i)
-
 -- | move the input file to the destination folder
 --
 -- >>> inToCopy "somedir/otherdir/file.ext"
 -- "output/otherdir/file/input.ext"
+inToCopy :: FilePath -> FilePath
 inToCopy i = joinPath [fileToFolder i, inputName i]
-
 -- | convert the input file to the output file
 --
 -- >>> inToOut "newExt"
 -- "document.newExt"
+inToOut :: FilePath -> FilePath
 inToOut = addExtension "document"
 
-version = "pandoc"
+-- paths
+--
 --version = offset <> "fork" -- in order to use a local fork
+linker = unpack offset <> "xmLeges-Linker-1.13a.exe"
+doc = "document.rst" :: FilePath
+-- the following are for troubleshooting
+docNative = "document.native" :: FilePath
+docUnlinked = "document-unlinked.rst" :: FilePath
 
--- | translate applying most filters
-makeDocument it = do
-  shell toRST empty
-  shell toNative empty
-  where toRST = pack $ command doc
-        toNative = pack $ command docNative
-        command d = version <> " " <> (x inFile) <> " " <> opts <> " -o " <> d
-        inFile = inputName i
-        i = unpack it
-        x f = "\"" <> f <> "\""
-
-linker = offset <> "xmLeges-Linker-1.13a.exe"
-doc = "document.rst" :: String
-docNative = "document.native" :: String -- for troubleshooting
-docUnlinked = "document-unlinked.rst" :: String
-
-linkNormattiva = do
-  t <- doesFileExist linker
-  when t (do
-    renameFile doc docUnlinked
-    void $ shell (pack command) empty
-    )
-  where command = version <> " " <> docUnlinked <> " -t html | " <> linker <> " | pandoc -f html -o " <> doc <> " " <> writeOpts
-
-
-makeSphinx = do
-  shell (pack command) empty
-  shell "test -e media && cp -r media index" empty -- for Sphinx
-    where command = version <> " " <> doc <> " --wrap none -o index.rst " <> addFilter "to-sphinx.hs"
+-- commands
+--
+version = "pandoc" -- change to switch the executable name everywhere
+-- the following fun could become second order if needed
+inputNameText :: Text -> Text
+inputNameText = pack . inputName . unpack
+-- | translate applying most filters. we try to work in Text
+makeDocument :: Text -> Text
+makeDocument d = version <> " input.docx " <> opts <> " -o " <> d
+toRST = makeDocument $ pack doc :: Text
+toNative = makeDocument $ pack docNative :: Text
+makeSphinx = version <> " " <> pack doc <> " --wrap none -o index.rst " <> addFilter "to-sphinx.hs"
+linkNormattiva = version <> " " <> pack docUnlinked <> " -t html | " <> pack linker <> " | pandoc -f html -o " <> pack doc <> " " <> writeOpts
 
 main = do
-  (d) <- options "translate DOCX file" parser
-  createDirectoryIfMissing True (fileToFolder (unpack d))
-  copyFile (unpack d) (inToCopy (unpack d))
-  withCurrentDirectory (fileToFolder (unpack d)) (do
-      makeDocument d
-      linkNormattiva
-      makeSphinx
-    )
+  (docArg) <- options "translate DOCX file" parser
+  convertDocsItalia (unpack docArg)
 
+-- this function is a good high-lever description of the logic
+convertDocsItalia :: System.FilePath.Posix.FilePath -> IO ()
+convertDocsItalia d = do
+  createDirectoryIfMissing True (fileToFolder d)
+  copyFile d (inToCopy d)
+  void $ withCurrentDirectory (fileToFolder d) (do
+    mys toRST
+    mys toNative
+    t <- doesFileExist linker
+    when t (do
+      renameFile doc docUnlinked
+      void $ mys linkNormattiva
+      )
+    mys makeSphinx
+    mys "test -e media && cp -r media index" -- for Sphinx
+    )
+  where mys c = shell c empty -- for readability
 
 
