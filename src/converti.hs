64ab3c96199b1912bf93490f411738dcfa46aca1
danse <f.occhipinti@gmail.com>
2018-07-13 10:53:04 +0200

    i tipi tornano

--- src/converti.hs
+++ src/converti.hs
@@ -4,17 +4,96 @@ import Turtle hiding (splitDirectories,
                       replaceExtensions,
                       stdout,
                       stderr,
-                      FilePath(..)
+                      FilePath(..),
+                      options,
+                      option,
+                      switch,
+                      die
                      )
 import Data.Text (pack, unpack, intercalate)
 import System.FilePath.Posix
 import System.Directory
 import Data.Maybe (isJust)
+import Options.Applicative
+import Data.Aeson hiding (Options)
+import System.Exit
+
+import qualified Data.ByteString.Lazy as B
+
+data UserOptions = UserOptions {
+  documentoCommandOption :: Maybe String,
+  collegamentoNormativaCommandOption :: Maybe Bool,
+  celleComplesseCommandOption :: Maybe Bool,
+  preservaCitazioniCommandOption :: Maybe Bool,
+  dividiSezioniCommandOption :: Maybe Bool
+  }
+
+instance FromJSON UserOptions where
+  parseJSON = withObject "UserOptions" $ \ v -> UserOptions
+    <$> v .:? "documento"
+    <*> v .:? "collegamento-normativa"
+    <*> v .:? "celle-complesse"
+    <*> v .:? "preserva-citazioni"
+    <*> v .:? "dividi-sezioni"
+
+data Options = Options {
+  documentoOption :: String,
+  collegamentoNormativaOption :: Bool,
+  celleComplesseOption :: Bool,
+  preservaCitazioniOption :: Bool,
+  dividiSezioniOption :: Bool
+  }
+
+applyDefaults :: UserOptions -> Options
+applyDefaults o = Options {
+  documentoOption = def "documento.docx" $ documentoCommandOption o,
+  collegamentoNormativaOption = def False $ collegamentoNormativaCommandOption o,
+  celleComplesseOption = def False $ celleComplesseCommandOption o,
+  preservaCitazioniOption = def False $ preservaCitazioniCommandOption o,
+  dividiSezioniOption = def False $ dividiSezioniCommandOption o
+  }
+
+data CommandLineOptions = DirectOptions UserOptions | JsonOptions String
+
+options = commandLineOptions <|> jsonOptions
+
+jsonOptions :: Parser CommandLineOptions
+jsonOptions = JsonOptions <$> option str (
+  long "opzioni-json"
+  <> help "permette di indicare un file JSON da cui leggere le opzioni di converti")
+
+commandLineOptions :: Parser CommandLineOptions
+commandLineOptions = DirectOptions <$> (UserOptions
+          <$> optional (argument str (metavar "documento.ext"))
+          <*> optional (switch (long "collegamento-normativa"
+                               <> help "sostituisce i riferimenti alle leggi con links a Normattiva"
+                               <> showDefault))
+          <*> optional (switch (long "celle-complesse"
+                               <> help "evita errori nelle celle di tabella complesse scrivendo righe molto lunghe"
+                               <> showDefault))
+          <*> optional (switch (long "preserva-citazioni"
+                               <> help "evita di rimuovere le citazioni"
+                               <> showDefault))
+          <*> optional (switch (long "dividi-sezioni"
+                               <> help "produce un file .rst per ogni capitolo"
+                               <> showDefault)))
+
+main = do
+  opts <- execParser (info options fullDesc)
+  userOptions <- case opts of
+    DirectOptions o -> pure o
+    JsonOptions json -> do
+      b <- B.readFile json
+      case (decode b) of
+        Nothing -> die "Error parsing the JSON option file"
+        Just o -> return o
+  converti (applyDefaults userOptions)
 
 maybeHead [] = Nothing
 maybeHead l = Just (head l)
 
 -- default
+def :: a -> Maybe a -> a
 def d Nothing = d
 def d (Just something) = something
 
@@ -98,10 +177,6 @@ toNative i = convert i (pack docNative :: Text)
 makeSphinx = "pandoc -t json " <> pack doc <> " | pandoc-to-sphinx"
 linkNormattiva = version <> " " <> pack docUnlinked <> " -t html | " <> pack linker <> " | pandoc -f html -o " <> pack doc <> " " <> writeOpts
 
-main = do
-  (docArg) <- options "translate DOCX file" parser
-  convertDocsItalia (unpack docArg)
-
 checkExecutables = do
   maybeExecutables <- sequence $ map findExecutable allFilters'
   maybeNotify (dropWhile isJust maybeExecutables)
@@ -113,13 +188,13 @@ maybeNotify missing  = print (errore $ head missing)
   -- where errore c = "`converti` si basa sul comando " <> c <> " che non è disponibile sul tuo sistema. Puoi installarlo seguendo le istruzioni che trovi su https://github.com/italia/docs-italia-pandoc-filters"
 
 -- this function is a good high-level description of the logic
-convertDocsItalia :: System.FilePath.Posix.FilePath -> IO ()
-convertDocsItalia i = do
+converti :: Options -> IO ()
+converti opts = do
   checkExecutables
-  createDirectoryIfMissing True (fileToFolder i)
-  copyFile i (inToCopy i)
-  void $ withCurrentDirectory (fileToFolder i) (do
-    mys (toRST i)
+  createDirectoryIfMissing True (fileToFolder d)
+  copyFile d (inToCopy d)
+  void $ withCurrentDirectory (fileToFolder d) (do
+    mys (toRST d)
     --mys toNative
     maybeLinker <- findExecutable linker
     when (isJust maybeLinker) (do
@@ -128,6 +203,7 @@ convertDocsItalia i = do
       )
     mys makeSphinx
     )
-  where mys c = shell c empty -- for readability
+  where d = documentoOption opts
+        mys c = shell c empty -- for readability
 
 
